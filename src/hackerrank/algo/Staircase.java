f42107c8a5f5d28e17b7afe45310b0992518d4cb
bha1 <peeyushbhawan@gmail.com>
2017-11-07 00:49:23 +1100

    Hackerrank staircase

---
+++ src/hackerrank/algo/Staircase.java
@@ -0,0 +1,18 @@
+package hackerrank.algo;
+
+import java.util.Scanner;
+
+public class Staircase {
+    public static void printPattern(int n){
+        for(int i = 5;i>=0;i--){
+        //	System.out.println();
+        	System.out.println(String.format("%"+(n)+"s",String.format("%-"+(n-i)+"s","").replaceAll(" ","#") ));
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        printPattern(n);
+    }
+}
