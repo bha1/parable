bbb328e0e012b95d158c339af6030e307ba09eb1
bha1 <peeyushbhawan@gmail.com>
2017-11-08 00:51:57 +1100

    shortcut inspite of writing code snippets just copy paste the text

--- src/intrfac/vs/clazz/absclass/DemoC1.java
+++ src/intrfac/vs/clazz/absclass/DemoC1.java
@@ -4,7 +4,12 @@ package intrfac.vs.clazz.absclass;
 /**
  * @author bhawan
  * Difference 1 : Astract class can extend only one class or obstract class
- *
+ *1 	 An abstract class can extend only one class or one abstract class at a time 	 An interface can extend any number of interfaces at a time
+ *2 	  An abstract class can extend another concrete (regular) class or abstract class 	 An interface can only extend another interface
+ *3 	 An abstract class can have both abstract and concrete methods 	 An interface can have only abstract methods
+ *4 	 In abstract class keyword “abstract” is mandatory to declare a method as an abstract 	 In an interface keyword “abstract” is optional to declare a method as an abstract
+ *5 	 An abstract class can have protected and public abstract methods 	 An interface can have only have public abstract methods
+ *6 	 An abstract class can have static, final or static final variable with any access specifier 	 interface can only have public static final (constant) variable
  */
 public class DemoC1 {
 	public static void main(String[] args){
